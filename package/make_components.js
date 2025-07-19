56eaf5d14d2ec7ca66a8ec5a87129ffa0134e5aa
masdevan <masdevansugiharta@gmail.com>
2025-07-20 01:26:13 +0700

    update components support custom attribute

--- package/make_components.js
+++ package/make_components.js
@@ -2,25 +2,22 @@
 const fs = require('fs');
 const path = require('path');
 
+//===== Custom Attribut =====//
+function parseAttributes(str) {
+    const attrs = {};
+    const attrRegex = /([a-zA-Z0-9_\-]+)\s*=\s*"([^"]*)"/g;
+    let match;
+    while ((match = attrRegex.exec(str))) {
+        attrs[match[1]] = match[2];
+    }
+    return attrs;
+}
+
 //===== Server Main Code =====//
 function renderWithComponents(filePath) {
     let html = fs.readFileSync(filePath, 'utf-8');
 
-    html = html.replace(/<([a-zA-Z0-9_\-\.]+)\s*\/>/g, (match, tagName) => {
-        let compPath;
-        if (tagName.includes('.')) {
-            const parts = tagName.split('.');
-            compPath = path.join(path.dirname(filePath), parts[0], `${parts[1]}.html`);
-        } else {
-            compPath = path.join(path.dirname(filePath), `${tagName}.html`);
-        }
-        if (fs.existsSync(compPath)) {
-            return fs.readFileSync(compPath, 'utf-8');
-        }
-        return match;
-    });
-
-    html = html.replace(/<([a-zA-Z0-9_\-\.]+)>([\s\S]*?)<\/\1>/g, (match, tagName, innerContent) => {
+    html = html.replace(/<([a-zA-Z0-9_\-\.]+)([^>]*)\/>/g, (match, tagName, attrStr) => {
         let compPath;
         if (tagName.includes('.')) {
             const parts = tagName.split('.');
@@ -30,6 +27,29 @@ function renderWithComponents(filePath) {
         }
         if (fs.existsSync(compPath)) {
             let compHtml = fs.readFileSync(compPath, 'utf-8');
+            const attrs = parseAttributes(attrStr);
+            for (const key in attrs) {
+                compHtml = compHtml.replace(new RegExp(`\{insert\.${key}\}`, 'g'), attrs[key]);
+            }
+            return compHtml;
+        }
+        return match;
+    });
+
+    html = html.replace(/<([a-zA-Z0-9_\-\.]+)([^>]*)>([\s\S]*?)<\/\1>/g, (match, tagName, attrStr, innerContent) => {
+        let compPath;
+        if (tagName.includes('.')) {
+            const parts = tagName.split('.');
+            compPath = path.join(path.dirname(filePath), parts[0], `${parts[1]}.html`);
+        } else {
+            compPath = path.join(path.dirname(filePath), `${tagName}.html`);
+        }
+        if (fs.existsSync(compPath)) {
+            let compHtml = fs.readFileSync(compPath, 'utf-8');
+            const attrs = parseAttributes(attrStr);
+            for (const key in attrs) {
+                compHtml = compHtml.replace(new RegExp(`\{insert\.${key}\}`, 'g'), attrs[key]);
+            }
             if (/<h:slot\s*\/?>([\s\S]*?)?/i.test(compHtml)) {
                 compHtml = compHtml.replace(/<h:slot\s*\/?>([\s\S]*?)?/i, innerContent);
                 return compHtml;
